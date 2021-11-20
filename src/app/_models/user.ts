536a2939f59914cf6bc840300fbe0b341c25e653
Andrey Babayev <77857802+AndreyBabayev@users.noreply.github.com>
2021-11-20 14:40:30 +0200

    myproject-car

---
+++ src/app/_models/user.ts
@@ -0,0 +1,11 @@
+﻿export class User {
+    id!: string;
+    title!: string;
+    firstName!: string;
+    lastName!: string;
+    carNumber!: string;
+    carName!: string;
+    carVin!: string;
+    email!: string;
+    isDeleting: boolean = false;
+}
